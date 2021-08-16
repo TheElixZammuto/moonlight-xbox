71566c648780f3117ed4d1e77e750533ef868eff
Elia Zammuto <theelixzammuto@gmail.com>
2021-08-16 18:10:07 +0200

    Removed moonlight-common-c from the repository

--- third_party/moonlight-common-c/build-uwp.bat
+++
@@ -1,1 +0,0 @@
-cmake -b build -S . -DCMAKE_TOOLCHAIN_FILE=..\..\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-uwp -G "Visual Studio 16 2019" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION="10.0" -DTARGET_UWP=ON
