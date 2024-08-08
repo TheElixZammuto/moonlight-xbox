bdffb80e9cddf69d17dd272a57cf6f3fb2dfb32f
Brady Brenot <brady@brenot.ca>
2024-08-08 01:51:30 -0400

    Mitigate corruption, ask for an IDR every X frames
    To mitigate #117 (Compression artifacts forming when left on the same scene for a while), request an IDR frame from the host every 1000 packets. This seems to reset the decoder well enough to completely avoid slow corruption problem, but shouldn't add too much extra bandwidth.
    
    Unless I'm missing something, Sunshine never sends I or IDR frames after initially establishing the stream unless it's specifically asked to by the client.

--- Streaming/FFmpegDecoder.cpp
+++ Streaming/FFmpegDecoder.cpp
@@ -14,7 +14,9 @@ extern "C" {
 #include<libswscale/swscale.h>
 #include<libavutil/hwcontext_d3d11va.h>
 }
+
 #define DECODER_BUFFER_SIZE 1048576
+#define MAX_DELAY_BETWEEN_IDR_FRAMES 1000
 
 namespace moonlight_xbox_dx {
 
@@ -163,6 +165,9 @@ namespace moonlight_xbox_dx {
 			Utils::Log("Using hack for Xbox One Consoles");
 			hackWait = true;
 		}
+
+		framesSinceLastIDR = 0;
+
 		return 0;
 	}
 
@@ -205,12 +210,21 @@ namespace moonlight_xbox_dx {
 			entry = entry->next;
 		}
 		int err;
+
 		err = Decode(ffmpeg_buffer, length);
 		if (err < 0) {
 			LiCompleteVideoFrame(frameHandle, DR_NEED_IDR);
 			return false;
 		}
+
 		LiCompleteVideoFrame(frameHandle, DR_OK);
+
+		if (framesSinceLastIDR++ >= MAX_DELAY_BETWEEN_IDR_FRAMES) {
+			// This mitigates the issue where the video slowly gets more corrupt
+			LiRequestIdrFrame();
+			framesSinceLastIDR = 0;
+		}
+
 		return true;
 	}
 
